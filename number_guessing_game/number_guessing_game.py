a273bfacfdcd9316755bd32deaa92f5ae800ad71
MrKindness235 <patriciocoitino235@gmail.com>
2024-04-03 10:36:46 -0700

    Working in a new quiz game project.

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 """ A random number guessing game"""
-pass
+
+import random
+
+
---
+++ quiz_game/quiz.game.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A small quiz game"""
+
+# Questions:
+
+questions = [
+    {
+        "prompt": "What is the capital of France?",
+        "options": ["A. Paris", "B. London", "C. Berlin", "D. Madrid"],
+        "answer": "A"
+    },
+
+    {
+        "prompt": "Which language is primarily spoken in Brazil?",
+        "options": ["A. Spanish", "B. Portuguese", "C. English", "D. French"],
+        "answer": "B"
+    },
+
+    {
+        "prompt": "What is the smallest prime number?",
+        "options": ["A. 1", "B. 2", "C. 3", "D. 5"],
+        "answer": "B"
+    },
+
+    {
+        "prompt": "Who wrote 'To Kill a Mockingbird?",
+        "options": ["A. Harper Lee", "B. Mark Twain", "C. J. K. Rowling",
+                    "D. Ernest Hemingway"],
+        "answer": "A"
+    },
+]
+
+# Start of question function:
+
+
+def runquiz(questions):
+    score = 0
+    for question in questions:
+        print(question["prompt"])
+        for option in question["options"]:
+            print(option)
