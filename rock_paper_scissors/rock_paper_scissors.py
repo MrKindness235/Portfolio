5a35f019c8e6c982ec803f370dd5b743c1e421cb
MrKindness235 <patriciocoitino235@gmail.com>
2024-08-27 09:31:01 -0700

    Rock, Paper, Scissors Arcade game (Elias) is ready!

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -6,14 +6,60 @@ import random
 
 choices = ['rock', 'paper', 'scissors']
 
-compchoice = random.choice(choices)
-pchoice = input("Enter rock, paper, or scissors").lower
+player_points = 0
+comp_points = 0
 
-if pchoice == "rock" and compchoice == "paper":
-    print("I win!")
+print("Welcome to the Rock, Paper and Scissors arcade game!\n\
+My name is Elias, let's have fun!")
+while True:
+    comp_choice = random.choice(choices)
+    player_choice = input("Enter rock, paper, or scissors. \
+Type exit to finish the game: ").lower()
 
-elif pchoice == "paper" and compchoice == "scissors":
-    print("I win!")
+    if player_choice == "rock" and comp_choice == "paper":
+        comp_points += 1
+        print(f"I choose {comp_choice}")
+        print("I win!")
+        print(f"Score: You: {player_points} - Me: {comp_points}")
 
-elif pchoice == "scissors" and compchoice == "rock":
-    print("I win!")
+    elif player_choice == "paper" and comp_choice == "scissors":
+        comp_points += 1
+        print(f"I choose {comp_choice}")
+        print("I win!")
+        print(f"Score: You: {player_points} - Me: {comp_points}")
+
+    elif player_choice == "scissors" and comp_choice == "rock":
+        comp_points += 1
+        print(f"I choose {comp_choice}")
+        print("I win!")
+        print(f"Score: You: {player_points} - Me: {comp_points}")
+
+    elif player_choice == "scissors" and comp_choice == "paper":
+        player_points += 1
+        print(f"I choose {comp_choice}")
+        print("You win!")
+        print(f"Score: You: {player_points} - Me: {comp_points}")
+
+    elif player_choice == "rock" and comp_choice == "scissors":
+        player_points += 1
+        print(f"I choose {comp_choice}")
+        print("You win!")
+        print(f"Score: You: {player_points} - Me: {comp_points}")
+
+    elif player_choice == "paper" and comp_choice == "rock":
+        player_points += 1
+        print(f"I choose {comp_choice}")
+        print("You win!")
+        print(f"Score: You: {player_points} - Me: {comp_points}")
+
+    elif player_choice == comp_choice:
+        print(f"I choose {comp_choice}")
+        print("Stalemate!")
+        print(f"Score: You: {player_points} - Me: {comp_points}")
+
+    elif player_choice == "exit":
+        print("Thank you for playing")
+        break
+
+    else:
+        print("Couldn't get that. Try again.")
